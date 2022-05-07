d961103d6546dc4cb73987742a3ba28ee19ca28d
Mukesh Bishnoi <mukeshbishnoi@gmail.com>
2022-05-07 19:11:12 +0530

    login data state added

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import {
   Box,
   Button,
@@ -11,18 +12,26 @@ import { ArrowForwardIcon } from '@chakra-ui/icons';
 import { Link as ReachLink } from 'react-router-dom';
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  });
+  const submitFormHandler = userData => {
+    console.log('Submitted!', userData);
+  };
+
   return (
     <Box display="flex" justifyContent="center">
       <FormControl w="40%">
         <FormLabel htmlFor="email" my="4">
           Email address
         </FormLabel>
-        <Input id="email" type="email" />
+        <Input id="email" type="email" onChange={e => setUser({ ...user, email: e.target.value })} value={user.email}/>
         <FormHelperText>We'll never share your email.</FormHelperText>
         <FormLabel htmlFor="password" my="4">
           Password
         </FormLabel>
-        <Input id="password" type="password" />
+        <Input id="password" onChange={e => setUser({ ...user, password: e.target.value })} type="password" value={user.password}/>
         <FormHelperText>We'll never share your password.</FormHelperText>
         <Box display="flex" alignItems="center" justifyContent="space-between">
           <Button
@@ -30,6 +39,7 @@ const Login = () => {
             rightIcon={<ArrowForwardIcon />}
             colorScheme="blue"
             my="4"
+            onClick={() => submitFormHandler(user)}
           >
             Login
           </Button>
