7803d9b477896bd983f9877cb8cc30257fed7f03
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-05-07 15:24:05 +0530

    feat: add navbar and logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { ChakraProvider, Box, theme } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Routes, Route } from 'react-router-dom';
 import { Login, SignUp } from './Pages';
+import { Navbar } from './components';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box fontSize="xl">
-        <ColorModeSwitcher justifySelf="flex-end" />
+        <Navbar />
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
