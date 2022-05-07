4f50a9e4f28c8e028ba887d94bec19c247f63fde
Mukesh Bishnoi <mukeshbishnoi@gmail.com>
2022-05-08 01:52:56 +0530

    feat: protected route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import { ChakraProvider, Box, theme } from '@chakra-ui/react';
 import { Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
 import { Login, SignUp, Home, Profile } from './pages';
-import { Navbar } from './components';
-=======
-import { Login, SignUp, Home } from './pages';
 import { Navbar, ProtectedRoute } from './components';
->>>>>>> ce8c40b935519314f5bb7e6b11286ee5b856a1b6
 
 function App() {
   return (
@@ -17,11 +12,9 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/" element={<Home />} />
-<<<<<<< HEAD
-          <Route path="/profile" element={<Profile />} />
-=======
-          <Route element={<ProtectedRoute />}></Route>
->>>>>>> ce8c40b935519314f5bb7e6b11286ee5b856a1b6
+          <Route element={<ProtectedRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
         </Routes>
       </Box>
     </ChakraProvider>
